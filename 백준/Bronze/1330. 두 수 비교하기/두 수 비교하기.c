6c10159c970672b44812fde4f20762e454ffa23c
ensup <ensup@outlook.com>
2024-04-29 17:10:15 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+    int A;
+    int B;
+    scanf("%d %d",&A ,&B );
+    if (A>B){
+        printf(">\n");
+    }
+    if (A==B){
+        printf("==\n");
+    }
+    if (A<B){
+        printf("<\n");
+    }
+
+    return 0;
+}
+//어떻게 사이트 이름이 백준이냐 ㅋㅋ
