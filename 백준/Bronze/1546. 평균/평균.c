13310e9b9f954c1c2c21fccbe6c5d6e7acaaf27a
ensup <ensup@outlook.com>
2024-04-29 17:11:10 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+    float mex;
+    int sz;
+    scanf("%d",&sz);
+	float m[sz];
+	for(int i=0; i<sz; i++)
+	{
+		scanf("%f", &m[i]);
+	}
+    mex = m[0];
+	for (int i = 0; i < sz; i++) {
+		mex = mex < m[i] ? m[i] : mex;
+	}
+    float newscore;
+    for (int i = 0; i < sz; i++) {
+		newscore+=m[i]/mex*100;
+	}
+    printf("%f\n",newscore/sz);
+
+
+	return 0;
+}
