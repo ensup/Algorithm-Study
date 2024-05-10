cff19c5c39efc59292728fc1c12b98b13bcfa75f
ensup <ensup@chungbuk.ac.kr>
2024-05-10 22:29:03 +0900

    Create olymp-decrypt.c

---
+++ C/olymp-decrypt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//https://edu-coding.tistory.com/268
+#include <string.h>
+int main(){
+    char key[27];
+    char str[100];
+    char dec[100];
+    int len;
+    scanf("%s", &key);
+    getchar();
+    gets(str);
+    len = strlen(str);
+    for (int i = 0; i < 100; i++) {
+        int k;
+        if (str[i] >= 97 && str[i] <= 122) {
+            k = str[i] - 97;
+            dec[i] = key[k];
+        }
+        else if (str[i] >= 65 && str[i] <= 90) {
+            k = str[i] - 65;
+            dec[i] = key[k] - 32;
+        }
+        else
+            dec[i] = str[i];
+    }
+    for (int i = 0; i < len; i++)
+        printf("%c", dec[i]);
+    printf("\n");
+    return 0;
+}
