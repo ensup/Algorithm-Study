5ba9df418b014abd38b6cfa52e4e5a4255df5076
Ensup Choi <ensup@outlook.com>
2024-11-13 00:14:26 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,15 +3,19 @@
 #define R 31
 #define M 1234567891
 using namespace std;
-int hasher(int len, const char* str) {
+long long hasher(int len, const char* str) {
     long long sum = 0;
+    long long r_power = 1;
     for (int i = 0; i < len; i++) {
-        sum += (str[i] - 96) * (double)pow(R, i);
+        sum += ((str[i] - 96) * (r_power%M));
+        sum %= M;
+        r_power = (r_power * R)%M;
     }
     return (sum % M);
 }
 int main(){
-    int len,result;
+    int len;
+    long long result;
     char str[55];
     cin >> len >> str;
     result = hasher(len, str);
